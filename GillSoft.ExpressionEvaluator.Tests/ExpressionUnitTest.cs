96d574ba4cb9697e516e75e11cdc4df74248b176
Vijay Gill <vijay.gill@hotmail.com>
2017-03-28 22:55:12 +0100

    Added unit test project. Will add loads of tests this weekend.

---
+++ GillSoft.ExpressionEvaluator.Tests/ExpressionUnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GillSoft.ExpressionEvaluator.Tests
+{
+    [TestClass]
+    public class ExpressionUnitTest
+    {
+        [TestMethod]
+        public void AddPositiveToPositive()
+        {
+            //arrange
+            var expr = new Expression();
+
+            //act
+            var result = expr.Evaluate("10 + 15");
+
+            //assert
+            Assert.AreEqual((double)25, result);
+        }
+
+        [TestMethod]
+        public void AddPositiveToNegative()
+        {
+            //arrange
+            var expr = new Expression();
+
+            //act
+            var result = expr.Evaluate("10 + -15");
+
+            //assert
+            Assert.AreEqual((double)-5, result);
+        }
+
+        [TestMethod]
+        public void NegativeToNegative()
+        {
+            //arrange
+            var expr = new Expression();
+
+            //act
+            var result = expr.Evaluate("-10 + -15");
+
+            //assert
+            Assert.AreEqual((double)-25, result);
+        }
+
+        [TestMethod]
+        public void NegativeToPositive()
+        {
+            //arrange
+            var expr = new Expression();
+
+            //act
+            var result = expr.Evaluate("-10 + 15");
+
+            //assert
+            Assert.AreEqual((double)5, result);
+        }
+
+    }
+}
