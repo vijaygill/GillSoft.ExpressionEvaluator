0d80d6dec2cf63d00b29f7fde29859d7d3e3e3ff
Vijay Gill <vijay.gill@hotmail.com>
2018-02-17 15:40:39 +0000

    Updated packages.

--- Grammar/build.cmd
+++ Grammar/build.cmd
@@ -5,8 +5,8 @@ setlocal
 set src_path=%~dp0
 set dest_path=%~dp0..\GillSoft.ExpressionEvaluator\AntlrGenerated
 
-java -jar ..\tools\antlr-4.6-complete.jar ..\Grammar\Expression.g4 -no-listener -Dlanguage=CSharp -visitor -o ./ -package GillSoft.ExpressionEvaluator
-java -jar ..\tools\antlr-4.6-complete.jar ..\Grammar\xpath.g4 -no-listener -Dlanguage=CSharp -visitor -o ./ -package GillSoft.ExpressionEvaluator
+java -jar ..\tools\antlr-4.7.1-complete.jar ..\Grammar\Expression.g4 -no-listener -Dlanguage=CSharp -visitor -o ./ -package GillSoft.ExpressionEvaluator
+java -jar ..\tools\antlr-4.7.1-complete.jar ..\Grammar\xpath.g4 -no-listener -Dlanguage=CSharp -visitor -o ./ -package GillSoft.ExpressionEvaluator
 
 move "%src_path%*.cs" "%dest_path%"
 del "%src_path%*.tokens"
